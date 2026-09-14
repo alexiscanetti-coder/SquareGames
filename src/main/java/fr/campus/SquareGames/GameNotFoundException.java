c01516a5a2b3733156d6e6ba09422b9b012521fb
alexiscanetti-coder <alexis.canetti@le-campus-numerique.fr>
2026-09-14 11:11:20 +0200

    :recycle: chore : Supprime le Javadoc redondant sur les exceptions métier

--- src/main/java/fr/campus/SquareGames/GameNotFoundException.java
+++ src/main/java/fr/campus/SquareGames/GameNotFoundException.java
@@ -5,9 +5,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.UUID;
 
-/**
- * Levée quand aucune partie ne correspond à l'identifiant demandé. Traduite en {@code 404}.
- */
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class GameNotFoundException extends RuntimeException {
 
